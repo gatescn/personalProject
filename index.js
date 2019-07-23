4a7fe9e62d653106bb3943cc3dadd499682f06d2
gatescn <gatescn@vcu.edu>
2019-07-23 15:07:11 -0400

    Added NavBar

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
+import NavBar from './components/NavBar';
 import './style.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <NavBar/>
       </div>
     );
   }
