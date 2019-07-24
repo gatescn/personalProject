c792c8e8721790d8eb2bb92235ba1344ab73e57e
gatescn <gatescn@vcu.edu>
2019-07-24 15:05:56 -0400

    added Grid Menu layout

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
-import NavBar from './components/NavBar';
+import MenuGrid from './components/MenuGrid';
 import './style.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar/>
+        <MenuGrid/>
       </div>
     );
   }
